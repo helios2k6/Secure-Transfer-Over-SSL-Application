0642ae72b4e5d3d2bc38d1857a4199bbbfdd0669
helios2k6 <helios2k6@gmail.com>
2011-07-12 18:01:09 -0400

    Finally settled on a GUI look

--- NetBeans/DesktopApplication1/src/views/MainWindow.java
+++ NetBeans/DesktopApplication1/src/views/MainWindow.java
@@ -30,84 +30,58 @@ public class MainWindow extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        transfersPanel = new javax.swing.JPanel();
-        jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
-        jPanel3 = new javax.swing.JPanel();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jMenu2 = new javax.swing.JMenu();
+        verticalSplitPane = new javax.swing.JSplitPane();
+        splitPaneNestedSplitPeerShareFolder = new javax.swing.JSplitPane();
+        splitPanePeerInfoAndLocalShare = new javax.swing.JSplitPane();
+        peerInfoPane = new javax.swing.JScrollPane();
+        localShareFolderPane = new javax.swing.JScrollPane();
+        peerShareFolderPane = new javax.swing.JScrollPane();
+        transferInfoPane = new javax.swing.JScrollPane();
+        menuBar = new javax.swing.JMenuBar();
+        fileMenu = new javax.swing.JMenu();
+        fileMenuCloseButton = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Secure Transfer Over SSL Application");
 
-        transfersPanel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        transfersPanel.setName("transfersPanel"); // NOI18N
+        verticalSplitPane.setDividerLocation(450);
+        verticalSplitPane.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
+        verticalSplitPane.setName("verticalSplitPane"); // NOI18N
 
-        javax.swing.GroupLayout transfersPanelLayout = new javax.swing.GroupLayout(transfersPanel);
-        transfersPanel.setLayout(transfersPanelLayout);
-        transfersPanelLayout.setHorizontalGroup(
-            transfersPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 945, Short.MAX_VALUE)
-        );
-        transfersPanelLayout.setVerticalGroup(
-            transfersPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
-        );
+        splitPaneNestedSplitPeerShareFolder.setDividerLocation(500);
+        splitPaneNestedSplitPeerShareFolder.setName("splitPaneNestedSplitPeerShareFolder"); // NOI18N
 
-        jPanel1.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel1.setName("jPanel1"); // NOI18N
+        splitPanePeerInfoAndLocalShare.setDividerLocation(150);
+        splitPanePeerInfoAndLocalShare.setName("splitPanePeerInfoAndLocalShare"); // NOI18N
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 150, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 411, Short.MAX_VALUE)
-        );
+        peerInfoPane.setName("peerInfoPane"); // NOI18N
+        splitPanePeerInfoAndLocalShare.setLeftComponent(peerInfoPane);
 
-        jPanel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel2.setName("jPanel2"); // NOI18N
+        localShareFolderPane.setName("localShareFolderPane"); // NOI18N
+        splitPanePeerInfoAndLocalShare.setRightComponent(localShareFolderPane);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 411, Short.MAX_VALUE)
-        );
+        splitPaneNestedSplitPeerShareFolder.setLeftComponent(splitPanePeerInfoAndLocalShare);
 
-        jPanel3.setBorder(javax.swing.BorderFactory.createEtchedBorder());
-        jPanel3.setName("jPanel3"); // NOI18N
+        peerShareFolderPane.setName("peerShareFolderPane"); // NOI18N
+        splitPaneNestedSplitPeerShareFolder.setRightComponent(peerShareFolderPane);
 
-        javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
-        jPanel3.setLayout(jPanel3Layout);
-        jPanel3Layout.setHorizontalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 375, Short.MAX_VALUE)
-        );
-        jPanel3Layout.setVerticalGroup(
-            jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 411, Short.MAX_VALUE)
-        );
+        verticalSplitPane.setTopComponent(splitPaneNestedSplitPeerShareFolder);
 
-        jMenuBar1.setName("jMenuBar1"); // NOI18N
+        transferInfoPane.setName("transferInfoPane"); // NOI18N
+        verticalSplitPane.setRightComponent(transferInfoPane);
 
-        jMenu1.setText("File");
-        jMenu1.setName("jMenu1"); // NOI18N
-        jMenuBar1.add(jMenu1);
+        menuBar.setName("menuBar"); // NOI18N
 
-        jMenu2.setText("Edit");
-        jMenu2.setName("jMenu2"); // NOI18N
-        jMenuBar1.add(jMenu2);
+        fileMenu.setText("File");
+        fileMenu.setName("fileMenu"); // NOI18N
 
-        setJMenuBar(jMenuBar1);
+        fileMenuCloseButton.setText("Close");
+        fileMenuCloseButton.setName("fileMenuCloseButton"); // NOI18N
+        fileMenu.add(fileMenuCloseButton);
+
+        menuBar.add(fileMenu);
+
+        setJMenuBar(menuBar);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -115,26 +89,14 @@ public class MainWindow extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(transfersPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(verticalSplitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 949, Short.MAX_VALUE)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+            .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(transfersPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(verticalSplitPane, javax.swing.GroupLayout.DEFAULT_SIZE, 525, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
@@ -153,12 +115,15 @@ public class MainWindow extends javax.swing.JFrame {
         });
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
-    private javax.swing.JPanel jPanel3;
-    private javax.swing.JPanel transfersPanel;
+    private javax.swing.JMenu fileMenu;
+    private javax.swing.JMenuItem fileMenuCloseButton;
+    private javax.swing.JScrollPane localShareFolderPane;
+    private javax.swing.JMenuBar menuBar;
+    private javax.swing.JScrollPane peerInfoPane;
+    private javax.swing.JScrollPane peerShareFolderPane;
+    private javax.swing.JSplitPane splitPaneNestedSplitPeerShareFolder;
+    private javax.swing.JSplitPane splitPanePeerInfoAndLocalShare;
+    private javax.swing.JScrollPane transferInfoPane;
+    private javax.swing.JSplitPane verticalSplitPane;
     // End of variables declaration//GEN-END:variables
 }
